4e53875013b9635fe693f4dd16a9044ca37d0ffb
Rahul Singh <rahul.singh@anant.us>
2019-03-16 18:52:03 +0000

    minor changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Cassandra.Link',
   },
   plugins: [
     {
@@ -17,7 +17,7 @@ module.exports = {
       headers: {
         'Content-Type': 'application/json'
       },
-  
+
       // Request body
       data: {
 
@@ -101,12 +101,36 @@ module.exports = {
         name: `TTRS`,
         // Optional
         // Read parser document: https://github.com/bobby-brennan/rss-parser#readme
-        parserOption: {
+        /*parserOption: {
           customFields: {
             item: ['itunes:duration']
           }
-        }
+        }*/
       }
-    }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-657559-21",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        //exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Enables Google Optimize using your container Id
+        //optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // Enables Google Optimize Experiment ID
+        //experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+        // Set Variation ID. 0 for original 1,2,3....
+        //variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+        // Any additional create only fields (optional)
+        //sampleRate: 5,
+        //siteSpeedSampleRate: 10,
+        //cookieDomain: "example.com",
+      },
+    },
   ],
 }
