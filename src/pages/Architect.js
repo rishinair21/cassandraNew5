1f72fbb05201e63f9afd7aeb5b1d2bac5eb837e6
“Tanaka <tanaka.mapondera@anant.us>
2019-05-17 14:06:09 -0400

    removed markdown, added hardcode

---
+++ src/pages/Architect.js
@@ -0,0 +1,150 @@
+import React, { Component } from 'react'
+import Layout from "../components/layout";
+import './main.css';
+
+class Architect extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <section class="section">
+          <div class="container">
+            <div className="content">
+              <h1 id="framework">Framework</h1>
+                <p>A curated list of commercial tools, libraries, frameworks, open source software, and commercial software that we recommend and references to the &quot;awesome&quot; curated lists for those tools. Also suggested roadmaps for those who are part of our team to master the areas to become a Platform Architect. Maintained by Rahul Singh of <a href="http://anant.us">Anant</a>. Feel free contact me if you&#39;d like to collaborate on this and other awesome lists. <a href="https://github.com/Anant/awesome-cassandra">Awesome Cassandra</a> , <a href="https://github.com/Anant/awesome-solr">Awesome Solr</a>, <a href="https://github.com/Anant/awesome-lucene">Awesome Lucene</a></p>
+                <h2 id="contents">Contents</h2>
+                <ul>
+                <li><a href="#general">General</a> </li>
+                <li><a href="#interface">Interface</a></li>
+                <li><a href="#software">Software</a></li>
+                <li><a href="#bigdata">Big Data</a></li>
+                <li><a href="#system">System</a></li>
+                </ul>
+                <h2 id="general">General</h2>
+                <h3 id="software-architect-roadmap">Software Architect Roadmap</h3>
+                <ul>
+                <li>Knowledge Base<ul>
+                <li><a href="https://github.com/kamranahmedse/developer-roadmap/blob/master/images/frontend.png">Front End Development</a></li>
+                <li><a href="https://github.com/kamranahmedse/developer-roadmap/blob/master/images/backend.png">Back End Development</a><ul>
+                <li><a href="https://github.com/kamranahmedse/developer-roadmap/blob/master/images/devops.png">DevOps</a></li>
+                </ul>
+                </li>
+                </ul>
+                </li>
+                </ul>
+                <h2 id="interface">Interface</h2>
+                <h3 id="css">CSS</h3>
+                <ul>
+                <li><a href="https://github.com/troxler/awesome-css-frameworks">CSS Frameworks</a></li>
+                <li><a href="https://github.com/awesome-css-group/awesome-css">CSS Resources</a></li>
+                <li><a href="https://github.com/ikkou/awesome-css">CSS Libraries, Tools, Frameworks, Software</a></li>
+                </ul>
+                <h3 id="angular">Angular</h3>
+                <ul>
+                <li><a href="https://github.com/gdi2290/awesome-angular">Angular Resources</a></li>
+                <li><a href="https://github.com/brillout/awesome-angular-components">Angular 2+ Components &amp; Libraries</a></li>
+                </ul>
+                <h3 id="react">React</h3>
+                <ul>
+                <li><a href="https://github.com/enaqx/awesome-react">React Ecosystem</a></li>
+                <li><a href="https://github.com/brillout/awesome-react-components">React Component &amp; Libraries</a></li>
+                <li><a href="https://github.com/SaraVieira/awesome-react-platforms">React Components</a></li>
+                </ul>
+                <h2 id="software">Software</h2>
+                <h3 id="c">C</h3>
+                <ul>
+                <li><a href="https://github.com/uhub/awesome-c-sharp">C# Frameworks, Libraries &amp; Software</a></li>
+                </ul>
+                <h3 id="scala">Scala</h3>
+                <ul>
+                <li><a href="https://github.com/tindzk/awesome-scala-native">Scala Native Resources and Libraries</a></li>
+                </ul>
+                <h3 id="java">Java</h3>
+                <ul>
+                <li><a href="https://github.com/akullpp/awesome-java">Java Frameworks, Libraries &amp; Software</a></li>
+                </ul>
+                <h3 id="node">Node</h3>
+                <ul>
+                <li><a href="https://github.com/sindresorhus/awesome-nodejs">Node Packages &amp; Resources</a></li>
+                </ul>
+                <h3 id="python">Python</h3>
+                <ul>
+                <li><a href="https://github.com/ziadoz/awesome-php">Python Frameworks, Libraries, Software, Resources</a></li>
+                </ul>
+                <h3 id="php">PHP</h3>
+                <ul>
+                <li><a href="https://github.com/ziadoz/awesome-php">PHP Libraries, Resources, Shiny Things</a></li>
+                </ul>
+                <h2 id="bigdata">Big Data</h2>
+                <h3 id="database">Database</h3>
+                <ul>
+                <li><a href="https://github.com/sindresorhus/awesome">Database Resources</a></li>
+                </ul>
+                <h3 id="sql-server">SQL Server</h3>
+                <ul>
+                <li><a href="https://github.com/ktaranov/sqlserver-kit">SQL Server Resources</a></li>
+                </ul>
+                <h3 id="postgresql">PostgreSQL</h3>
+                <ul>
+                <li><a href="https://github.com/dhamaniasad/awesome-postgres">PostgreSQL Software, Libraries, Tools $ Resources</a></li>
+                </ul>
+                <h3 id="cassandra">Cassandra</h3>
+                <ul>
+                <li><a href="https://anant.github.io/awesome-cassandra/">Cassandra Libraries &amp; Resources</a></li>
+                </ul>
+                <h3 id="solr">Solr</h3>
+                <ul>
+                <li><a href="https://anant.github.io/awesome-solr/">Solr Libraries &amp; Resources</a></li>
+                </ul>
+                <h3 id="elasticsearch">Elasticsearch</h3>
+                <ul>
+                <li><a href="https://github.com/dzharii/awesome-elasticsearch">Elasticsearch Resources</a></li>
+                </ul>
+                <h3 id="graphql">GraphQL</h3>
+                <ul>
+                <li><a href="https://github.com/chentsulin/awesome-graphql">GraphQL Resources</a></li>
+                </ul>
+                <h2 id="system">System</h2>
+                <ul>
+                <li><a href="https://github.com/alexpate/awesome-design-systems">Design System Resources</a></li>
+                </ul>
+                <h3 id="container">Container</h3>
+                <ul>
+                <li><a href="https://github.com/tcnksm/awesome-container">Container Resources</a></li>
+                </ul>
+                <h3 id="container-orchestration">Container Orchestration</h3>
+                <ul>
+                <li><a href="https://github.com/Friz-zy/awesome-linux-containers">Container Orchestration Resources</a></li>
+                </ul>
+                <h3 id="infrastructure-automation">Infrastructure Automation</h3>
+                <ul>
+                <li><a href="https://github.com/atinfo/awesome-test-automation">Infrastructure Automation Resources</a></li>
+                </ul>
+                <h3 id="devops">DevOps</h3>
+                <ul>
+                <li><a href="https://github.com/mikewootini/awesome-devops">DevOps Resources</a></li>
+                </ul>
+                <h2 id="cloud-services">Cloud Services</h2>
+                <ul>
+                <li><a href="https://github.com/rootsongjc/awesome-cloud-native">Cloud Services Resources</a></li>
+                </ul>
+                <h3 id="saas">SaaS</h3>
+                <ul>
+                <li><a href="https://github.com/GetStream/awesome-saas-services">SaaS Resources</a></li>
+                </ul>
+                <h3 id="paas">PaaS</h3>
+                <ul>
+                <li><a href="">PaaS Resources</a></li>
+                </ul>
+                <h3 id="iaas">IaaS</h3>
+                <ul>
+                <li><a href="https://github.com/ramitsurana/awesome-openstack">IaaS Resources</a></li>
+                </ul>
+            </div>
+          </div>
+        </section>
+      </Layout>
+    )
+  }
+}
+
+export default Architect
